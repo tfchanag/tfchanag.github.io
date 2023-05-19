46c875410d42413fb17ab214d3229bb1da2d85ac
chantszf <chantszf@sheridancollege.ca>
2023-05-19 18:25:24 -0400

    Removed Sheridan

--- src/data/education-history.js
+++ src/data/education-history.js
@@ -1,12 +1,12 @@
 export const EducationHistory = [
-	{
-		"year": "2021 Sep - 2022 Dec",
-		"cert": "Diploma in Computer Programming",
-		"school": "Sheridan College, ON, Canada",
-		"key-courses-software": "Enterprise Java Development, Systems Dev. Methodologies, .NET using C#, Web Programming, Mobile Web-Based Application Development",
-		"key-courses-mech":"Object Oriented Program Java, Project Management",
-		"pic": require("../images/Education/sheridan.jpg")
-	},
+	// {
+	// 	"year": "2021 Sep - 2022 Dec",
+	// 	"cert": "Diploma in Computer Programming",
+	// 	"school": "Sheridan College, ON, Canada",
+	// 	"key-courses-software": "Enterprise Java Development, Systems Dev. Methodologies, .NET using C#, Web Programming, Mobile Web-Based Application Development",
+	// 	"key-courses-mech":"Object Oriented Program Java, Project Management",
+	// 	"pic": require("../images/Education/sheridan.jpg")
+	// },
 	{
 		"year": "2015 Sep - 2019 Jul",
 		"cert": "Bachelor of Engineering in Mechanical Engineering",
